515b1206fb06a7045ac02e1ea12f5d8bb7b03d12
rakshit9 <rakshitsarkheliya9@gmail.com>
2019-05-27 14:30:50 +0530

    Initial Commit,create d routing module, added hotel pages, added paths

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AccommodationComponent } from './accommodation/accommodation.component';
+import { FoodAndBeverageComponent } from './food-and-beverage/food-and-beverage.component';
+import { AuthComponent } from './auth/auth.component';
+import { CelebrationsComponent } from './celebrations/celebrations.component';
+import { LifestyleComponent } from './lifestyle/lifestyle.component';
+import { WeddingsComponent } from './weddings/weddings.component';
+import { OffersComponent } from './offers/offers.component';
+import { SigninComponent } from './auth/signin/signin.component';
+import { SignupComponent } from './auth/signup/signup.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'accommodation', component: AccommodationComponent},
+  {path: 'auth', component: AuthComponent},
+  {path: 'celebrations', component: CelebrationsComponent},
+  {path: 'food-and-beverage', component: FoodAndBeverageComponent},
+  {path: 'lifestyle', component: LifestyleComponent},
+  {path: 'offers', component: OffersComponent},
+  {path: 'wedding', component: WeddingsComponent},
+  {path: 'signin', component: SigninComponent},
+  {path: 'signup', component: SignupComponent},
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
